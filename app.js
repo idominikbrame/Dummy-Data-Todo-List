062ed9273a7bd1f0ef24b8859c26e575aeaf6867
Dominik Brame <idominikbrame@gmail.com>
2021-01-30 21:41:15 -0600

    filtered to do

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
 
+
+
 let arrayOfTodos = [
     {
     "userId": 14,
@@ -18,23 +20,59 @@ let arrayOfTodos = [
     fetch('https://jsonplaceholder.typicode.com/todos')
     .then( (response) => response.json())
     .then( (alert("fetch complete")))
-    .then( (json) => arrayOfTodos = json)
+    .then( (json) => arrayOfTodos = json);
   }
 
   const logTodos = () => {
-    console.log(arrayOfTodos)
+    console.log(arrayOfTodos);
   }
+  const currentlyStored = [];
 
   const populateTodos = () => {
-      for(let i = 0; i < arrayOfTodos.length - 180; i++) {
-        var ol = document.getElementById("todo-list");
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(arrayOfTodos[i].title));
-        ol.appendChild(li); 
-        if (arrayOfTodos[i].completed == false) {
-            ol.appendChild(li).style.color = "red";
-        } else if (arrayOfTodos[i].completed == true) {
-            ol.appendChild(li).style.color = "green"
-        }
+    reset()
+    let inputNumber = document.getElementById("idnumber").value;
+      for(let i = 0; i < arrayOfTodos.length; i++) {
+        if(arrayOfTodos[i].userId == inputNumber) {
+          currentlyStored.push(arrayOfTodos[i])
+          let ol = document.getElementById("todo-list");
+          let li = document.createElement("li");
+          li.appendChild(document.createTextNode(arrayOfTodos[i].title));
+          ol.appendChild(li);
+        } 
       }       
+  }
+  console.log(currentlyStored)
+
+
+  const showInputValue = () => {
+    let inputNumber = document.getElementById("idnumber").value;
+    console.log(inputNumber);
+  }
+
+  const reset = () => {
+    document.getElementById("todo-list").textContent = '';
+  }
+
+  const completed = () => {
+    reset()
+   for (let i = 0; i < currentlyStored.length; i++) {
+     if (currentlyStored[i].completed == true) {
+      let ol = document.getElementById("todo-list");
+          let li = document.createElement("li");
+          li.appendChild(document.createTextNode(arrayOfTodos[i].title));
+          ol.appendChild(li);
+     }
+   }
+  }
+
+  const notCompleted = () => {
+    reset()
+     for (let i = 0; i < currentlyStored.length; i++) {
+      if (currentlyStored[i].completed == false) {
+        let ol = document.getElementById("todo-list");
+          let li = document.createElement("li");
+          li.appendChild(document.createTextNode(arrayOfTodos[i].title));
+          ol.appendChild(li);
+     }
+   }
   }
